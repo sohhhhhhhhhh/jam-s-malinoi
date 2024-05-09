821305bed192c2031cd356806cfb8db929be2f7d
eMD0gG <vaspyrkov2@gmail.com>
2024-05-09 22:36:04 +1000

    эээ враг стреляет
    .

--- Assets/Scripts/EnemyAIArcher.cs
+++ Assets/Scripts/EnemyAIArcher.cs
@@ -1,8 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
-public class EnemyAIArcher : MonoBehaviour
-{
+public class EnemyAIArcher : MonoBehaviour {
+    public GameObject player;
+    public GameObject bullet;
+    public float fireRate;
+    private float timer;
+    private bool canFire = true;
+
     
+    void Start() {
+        timer = fireRate;
+    }
+
+    void Update() {
+        updateTimer();
+
+        if (canFire) {
+            Instantiate(bullet, transform.position, Quaternion.identity);
+            canFire = false;
+        }
+    }
+
+    void updateTimer() {
+        if (!canFire) {
+            timer -= 1 * Time.deltaTime;
+            if (timer <= 0) {
+                canFire = true;
+                timer = fireRate;
+            }
+        }
+    }
 }
