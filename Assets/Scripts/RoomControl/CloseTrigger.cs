4e8f28f3946a285093b2836aa687c477fd7b7614
ByterGame <vitazagrebelnyh@gmail.com>
2024-05-11 01:01:48 +1000

    stupidSpawner

---
+++ Assets/Scripts/RoomControl/CloseTrigger.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseTrigger : MonoBehaviour
+{
+    public static CloseTrigger Instance { get; private set; }
+    public GameObject closeDoor;
+    public bool spawnStart = false;
+    
+    void Awake()
+    {
+        Instance = this;
+    }
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
+            closeDoor.SetActive(true);
+            spawnStart = true;
+        }
+    }
+}
