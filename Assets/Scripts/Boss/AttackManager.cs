f7f9c9a30d07438c49e6d62a7761ccd46c1e9826
ByterGame <vitazagrebelnyh@gmail.com>
2024-05-12 01:54:25 +1000

    Baking

--- Assets/Scripts/Boss/AttackManager.cs
+++ Assets/Scripts/Boss/AttackManager.cs
@@ -2,18 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<< Updated upstream
 public class AttackManager : MonoBehaviour {
-    private Dictionary<Attack, float[]> AttackInfo;
-    [SerializeField] private FirstAttack _firstAttack;
-=======
-public class AttackManager : MonoBehaviour
-{
     private Dictionary<AttackParent, float[]> AttackInfo;
-    [SerializeField]private FirstAttack _firstAttack;
-    private float[] xd;
-    private AttackParent[] _attacks;
->>>>>>> Stashed changes
+    [SerializeField] private FirstAttack _firstAttack;
     private int currentAttackIndex = 0;
     private float NumOfAttacks = 1;
     private float NumOfWaves = 3;
@@ -27,17 +18,10 @@ public class AttackManager : MonoBehaviour
     private float timeUnderWave = 0.5f;
     private float timerUnderWave = 0.5f;
     private bool canUnderWave;
-<<<<<<< Updated upstream
 
     void Start() {
-=======
-    
-    void Start()
-    {
-        _attacks[0] = _firstAttack;
->>>>>>> Stashed changes
         AttackInfo[_firstAttack] = new float[]
-        { NumOfWaves, NumOfAttacks, remainingPatrons, timerWave, timeWave, timerUnderWave, timeUnderWave };
+            { NumOfWaves, NumOfAttacks, remainingPatrons, timerWave, timeWave, timerUnderWave, timeUnderWave };
     }
 
     void Update() {
@@ -49,14 +33,7 @@ public class AttackManager : MonoBehaviour
             UpdateUnderWaveTimer();
         }
 
-<<<<<<< Updated upstream
         if (canWave && canUnderWave) {
-=======
-        remainingPatrons = AttackInfo[_attacks[currentAttackIndex]][2];
-
-        if (canWave && canUnderWave)
-        {
->>>>>>> Stashed changes
             _firstAttack.Attack((remainingPatrons / 13) * 15);
             remainingPatrons--;
             if (remainingPatrons <= 0) {
@@ -88,21 +65,4 @@ public class AttackManager : MonoBehaviour
             timerUnderWave = timeUnderWave;
         }
     }
-<<<<<<< Updated upstream
 }
-=======
-
-    void updateVariable()
-    {
-        xd = AttackInfo[_attacks[currentAttackIndex]];
-
-        NumOfWaves = xd[0];
-        NumOfAttacks = xd[1];
-        remainingPatrons = xd[2];
-        timerWave = xd[3];
-        timeWave = xd[4];
-        timerUnderWave = xd[5];
-        timeUnderWave = xd[6];
-    }
-}
->>>>>>> Stashed changes
