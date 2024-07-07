753deebe93beaff9f73d590e97876258cf60ef7b
zxcpisya <mamin.papa0@gmail.com>
2024-07-07 14:01:24 +1000

    123
    123

--- Assets/Scripts/Boss/BossBehaviour.cs
+++ Assets/Scripts/Boss/BossBehaviour.cs
@@ -7,28 +7,43 @@ public class BossBehaviour : MonoBehaviour
     private int _hp = 20;
 
     private float _timer;
-    private float _speed;
-    private float _changeDirectionCooldown = 2f;
+    private float _speed = 2f;
+    private float _changeDirectionCooldown = 2;
     private int _currentDirectionIndex = 0;
-
+    private Rigidbody2D _rb;
     private Vector3[] _moveDirections =
     {
-        Vector3.back, Vector3.right, Vector3.forward, Vector3.left
+        Vector3.right, Vector3.up, Vector3.down, Vector3.left
     };
 
     private void Awake()
     {
         _timer = _changeDirectionCooldown;
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     private void Start()
     {
-        transform.Translate(_moveDirections[_currentDirectionIndex] * Time.deltaTime * _speed);
+        _rb.velocity = _moveDirections[_currentDirectionIndex] * _speed;
     }
 
     private void Update()
     {
         _timer -= Time.deltaTime;
+        print(_timer);
+        if (_timer < 0)
+        {
+            if (_currentDirectionIndex + 1 < 4)
+            {
+                _currentDirectionIndex += 1;
+            }
+            else
+            {
+                _currentDirectionIndex = 0;
+            }
+            _rb.velocity = _moveDirections[_currentDirectionIndex] * _speed;
+            _timer = _changeDirectionCooldown;
+        }
     }
 
     public void GetDamage(int damage)
